78a38f17fc1ccf04b9501db3526bc4934cc20301
tgfrerer <tim@poniesandlight.co.uk>
2014-01-16 10:44:56 +0000

    add additional checks before kernel work submissions
    * will round up to nearest multiple of local size to cover all of global size
    * will check whether total local workgroup size is within context's capability and not issue command but error if not.
    
    Signed-off-by: tgfrerer <tim@poniesandlight.co.uk>

--- src/MSAOpenCLKernel.cpp
+++ src/MSAOpenCLKernel.cpp
@@ -36,10 +36,20 @@ namespace msa {
 		err = clEnqueueNDRangeKernel(pOpenCL->getQueue(), clKernel, numDimensions, NULL, globalSize, localSize, 0, NULL, NULL);
 		assert(err == CL_SUCCESS);
 	}
+
+
+	inline size_t roundToNextMultipleOf(size_t num_, size_t divisor_){
+		return std::min(num_ % divisor_, size_t(1)) * divisor_ + (num_ / divisor_) * divisor_;
+	}
 	
 	void OpenCLKernel::run1D(size_t globalSize, size_t localSize) {
 		size_t globalSizes[1];
-		globalSizes[0] = globalSize;
+		// tig: make sure localSize <= maxWorkGroupSize
+		if (localSize > pOpenCL->info.maxWorkGroupSize) {
+			ofLogError() << "Could not run OpenCL 1D kernel at workgroup size: " << localSize << ". Max supported local (=workgroup) size: " << pOpenCL->info.maxWorkGroupSize;
+			return;
+		}
+		globalSizes[0] = roundToNextMultipleOf(globalSize,localSize);	// make sure global size is a multiple of local size
 		if(localSize) {
 			size_t localSizes[1];
 			localSizes[0] = localSize;
@@ -50,9 +60,14 @@ namespace msa {
 	}
 	
 	void OpenCLKernel::run2D(size_t globalSizeX, size_t globalSizeY, size_t localSizeX, size_t localSizeY) {
+		// tig: make sure localSizeX * localSizeY <= maxWorkGroupSize
+		if (localSizeX * localSizeY > pOpenCL->info.maxWorkGroupSize) {
+			ofLogError() << "Could not run OpenCL 2D kernel at workgroup size: " << localSizeY * localSizeX << ". Max supported local (=workgroup) size: " << pOpenCL->info.maxWorkGroupSize;
+			return;
+		}
 		size_t globalSizes[2];
-		globalSizes[0] = globalSizeX;
-		globalSizes[1] = globalSizeY;
+		globalSizes[0] = roundToNextMultipleOf(globalSizeX,localSizeX);	// make sure global size is a multiple of local size
+		globalSizes[1] = roundToNextMultipleOf(globalSizeY,localSizeY);
 		if(localSizeY && localSizeX) {
 			size_t localSizes[2];
 			localSizes[0] = localSizeX;
@@ -64,10 +79,15 @@ namespace msa {
 	}
 	
 	void OpenCLKernel::run3D(size_t globalSizeX, size_t globalSizeY, size_t globalSizeZ, size_t localSizeX, size_t localSizeY, size_t localSizeZ) {
+		// tig: make sure localSizeX * localSizeY * localSizeZ <= maxWorkGroupSize
+		if (localSizeX * localSizeY * localSizeZ > pOpenCL->info.maxWorkGroupSize) {
+			ofLogError() << "Could not run OpenCL 3D kernel at workgroup size: " << localSizeZ * localSizeY * localSizeX << ". Max supported local (=workgroup) size: " << pOpenCL->info.maxWorkGroupSize;
+			return;
+		}
 		size_t globalSizes[3];
-		globalSizes[0] = globalSizeX;
-		globalSizes[1] = globalSizeY;
-		globalSizes[2] = globalSizeZ;
+		globalSizes[0] = roundToNextMultipleOf(globalSizeX,localSizeX);	// make sure global sizes are a multiple of local size
+		globalSizes[1] = roundToNextMultipleOf(globalSizeY,localSizeY);
+		globalSizes[2] = roundToNextMultipleOf(globalSizeZ,localSizeZ);
 		if(localSizeZ && localSizeY && localSizeX) {
 			size_t localSizes[3];
 			localSizes[0] = localSizeX;
