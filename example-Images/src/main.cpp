ab2d8519161074166a2f5ad9cb5a33d826bdf86e
tgfrerer <tim@poniesandlight.co.uk>
2014-01-16 09:53:25 +0000

    update testApp -> ofApp filenames to conform to of 0.8 standards
    * also changed data dir to be held inside bin as in default oF
    
    Signed-off-by: tgfrerer <tim@poniesandlight.co.uk>

--- example-Images/src/main.cpp
+++ example-Images/src/main.cpp
@@ -1,16 +1,16 @@
 #include "ofMain.h"
-#include "testApp.h"
-#include "ofAppGlutWindow.h"
+#include "ofApp.h"
+
 
 //========================================================================
 int main( ){
 
-    ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1300,768, OF_WINDOW);			// <-------- setup the GL context
+	ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
+	ofSetupOpenGL( 1300,768, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofRunApp( new testApp());
+	ofRunApp( new ofApp());
 
 }
