69c25a30f6e82fdbb1f67cd39663170ed26795cd
Elie Zananiri <prisonerjohn@gmail.com>
2016-01-17 21:52:59 -0500

    No rounding global range when local range is NULL (This wasn't set for 3D).

--- src/MSAOpenCLKernel.cpp
+++ src/MSAOpenCLKernel.cpp
@@ -197,16 +197,20 @@ namespace msa {
 //			return;
 //		}
 		size_t globalSizes[3];
-		globalSizes[0] = roundToNextMultipleOf(globalSizeX,localSizeX);	// make sure global sizes are a multiple of local size
-		globalSizes[1] = roundToNextMultipleOf(globalSizeY,localSizeY);
-		globalSizes[2] = roundToNextMultipleOf(globalSizeZ,localSizeZ);
 		if(localSizeZ && localSizeY && localSizeX) {
+            globalSizes[0] = roundToNextMultipleOf(globalSizeX,localSizeX);	// make sure global sizes are a multiple of local size
+            globalSizes[1] = roundToNextMultipleOf(globalSizeY,localSizeY);
+            globalSizes[2] = roundToNextMultipleOf(globalSizeZ,localSizeZ);
 			size_t localSizes[3];
 			localSizes[0] = localSizeX;
 			localSizes[1] = localSizeY;
 			localSizes[2] = localSizeZ;
 			run(3, globalSizes, localSizes);
 		} else {
+            // no local size specified - let driver figure out how to break up workload.
+            globalSizes[0] = globalSizeX;
+            globalSizes[1] = globalSizeY;
+            globalSizes[2] = globalSizeZ;
 			run(3, globalSizes, NULL);
 		}
 	}
