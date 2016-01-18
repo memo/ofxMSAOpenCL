fac8ed218fc5823f8a7d0c8a4000698e747b2c68
Elie Zananiri <prisonerjohn@gmail.com>
2016-01-17 21:50:20 -0500

    Updated setting programmable renderer for OF 0.9.0

--- example-Hello World/src/main.cpp
+++ example-Hello World/src/main.cpp
@@ -5,12 +5,11 @@
 //========================================================================
 int main( ){
 
-	ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
-	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp( new ofApp());
+    ofGLWindowSettings settings;
+    settings.setGLVersion(3, 2);
+    settings.width = 1024;
+    settings.height = 768;
+    ofCreateWindow(settings);
+    ofRunApp(new ofApp());
 
 }
