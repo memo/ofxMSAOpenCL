bfc7ad4e31f0c7fa4cb8b1fa2962bcb20bf4df4e
Dimitre <dimitre.github@mailnull.com>
2016-04-25 14:44:13 -0300

    addon_config.mk
    now it adds OpenCL framework automatically with projectGenerator

---
+++ addon_config.mk
@@ -0,0 +1,9 @@
+meta:
+	ADDON_NAME = ofxMSAOpenCL
+	ADDON_DESCRIPTION = C++ openFrameworks addon for very simple to use wrapper for OpenCL. 
+	ADDON_AUTHOR = Memo Akten, www.memo.tv
+	ADDON_TAGS = "OpenCL"
+	ADDON_URL = https://github.com/memo/ofxMSAOpenCL
+
+osx:
+	ADDON_FRAMEWORKS = OpenCL
