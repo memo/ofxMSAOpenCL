941cdebe9ac70fdd2ec75f15d5b61e7446fc26ec
Memo Akten <memo@memo.tv>
2019-02-01 01:47:48 +0300

    updated addon_config to include linux lib

--- addon_config.mk
+++ addon_config.mk
@@ -1,9 +1,20 @@
 meta:
 	ADDON_NAME = ofxMSAOpenCL
-	ADDON_DESCRIPTION = C++ openFrameworks addon for very simple to use wrapper for OpenCL. 
+	ADDON_DESCRIPTION = C++ openFrameworks addon for very simple to use wrapper for OpenCL.
 	ADDON_AUTHOR = Memo Akten, www.memo.tv
 	ADDON_TAGS = "OpenCL"
 	ADDON_URL = https://github.com/memo/ofxMSAOpenCL
 
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	ADDON_DEPENDENCIES = ofxMSAPingPong
+
 osx:
 	ADDON_FRAMEWORKS = OpenCL
+
+linux64:
+	# any special flag that should be passed to the linker when using this
+	# addon, also used for system libraries with -lname
+	ADDON_LDFLAGS = -L/usr/local/cuda/lib64
+	ADDON_LDFLAGS +=-lOpenCL
